431c220129e5d934cb886882decb4d10bf013889
James Gardiner <jamesgardiner075@gmail.com>
2022-03-25 19:34:50 +0000

    Update add ingrediant test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, fireEvent, waitFor, screen } from "@testing-library/react"
+import { render, waitFor, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
 import App from "./App"
@@ -8,14 +8,13 @@ import App from "./App"
 test("I can add an ingrediant and it appears in the ingrediant list", async () => {
     render(<App />)
     const ingrediantName = "2 limes"
-    userEvent.type(
-        screen.getByPlaceholderText(
-            "Enter an ingrediant and quantity e.g. 2 lemons, cheese 500g"
-        ),
-        ingrediantName
-    )
+    const placeholderText =
+        "Enter an ingrediant and quantity e.g. 2 lemons, cheese 500g"
+    const inputElem = screen.getByPlaceholderText(placeholderText)
+    userEvent.type(inputElem, ingrediantName)
     userEvent.click(screen.getByRole("button", { name: /enter/i }))
     await waitFor(() => {
         expect(screen.getByText(ingrediantName)).not.toBeNull()
     })
+    expect(inputElem).toHaveValue("")
 })
