9925514775817a48093de62ef6a784025441c3e3
James Gardiner <jamesgardiner075@gmail.com>
2022-03-25 19:14:07 +0000

    Add test for adding ingrediants to list

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, waitFor, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from "./App"
+
+// Creator tests
+
+test("I can add an ingrediant and it appears in the ingrediant list", async () => {
+    render(<App />)
+    const ingrediantName = "2 limes"
+    userEvent.type(
+        screen.getByPlaceholderText(
+            "Enter an ingrediant and quantity e.g. 2 lemons, cheese 500g"
+        ),
+        ingrediantName
+    )
+    userEvent.click(screen.getByRole("button", { name: /enter/i }))
+    await waitFor(() => {
+        expect(screen.getByText(ingrediantName)).not.toBeNull()
+    })
+})
